b54552d2e6cc5dee6974a6be74916cc55eb1462d
Nadzeya <lustra2013@gmail.com>
2023-02-01 02:34:32 +0300

    add math_op

---
+++ math_op.py
@@ -0,0 +1,24 @@
+
+
+# вычисления
+
+def sum(a, b):
+    return f"{a} + {b} = {a + b}"
+    
+def sub(a, b):
+    return f"{a} - {b} = {a - b}"
+
+def mult(a, b):
+    return f"{a} * {b} = {a * b}"
+
+def div_rem(a, b):   
+        return f"{a} % {b} = {a % b}"
+    
+def div_all(a, b):                     #обычное деление
+    return f"{a} / {b} = {a / b}"
+    
+def div_int(a, b):                     #целочисленное деление
+    return f"{a} / {b} = {a / b}"
+
+def pow(a, b):
+    return f"{a}^{b} = {a ** b}"
